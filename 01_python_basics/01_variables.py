f12c38ebbc97ecd8c6d1c13d9b69188949f85ffa
Tamseelansari11 <ansaritamseel626@gmail.in>
2026-08-10 20:37:54 +0530

    Add variables.py

---
+++ 01_python_basics/01_variables.py
@@ -0,0 +1,23 @@
+"""
+ Variables in Python
+
+Variables are used to store data.
+Python automatically determines the data type based on the value assigned.
+"""
+
+# String
+player_name = "Lamine Yamal"
+
+# Integer
+age = 18
+
+# Float
+market_value = 200.5  # in million euros
+
+# Boolean
+is_starting_player = True
+
+print("Player Name:", player_name)
+print("Age:", age)
+print("Market Value (€M):", market_value)
+print("Starting Player:", is_starting_player)
