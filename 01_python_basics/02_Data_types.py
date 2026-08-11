b13ec256b479b62f5cd7da106e8efb58953ad878
Tamseelansari11 <ansaritamseel626@gmail.in>
2026-08-11 21:57:27 +0530

    Add data types .py file

---
+++ 01_python_basics/02_Data_types.py
@@ -0,0 +1,54 @@
+# ==========================================
+# Python Data Types
+# ==========================================
+
+# Integer
+age = 18
+print("Integer:", age)
+print("Type:", type(age))
+print()
+
+# Float
+height = 5.7
+print("Float:", height)
+print("Type:", type(height))
+print()
+
+# String
+name = "Mohammad Tamseel Ansari"
+print("String:", name)
+print("Type:", type(name))
+print()
+
+# Boolean
+is_student = True
+print("Boolean:", is_student)
+print("Type:", type(is_student))
+print()
+
+# List
+languages = ["Python", "Java", "C"]
+print("List:", languages)
+print("Type:", type(languages))
+print()
+
+# Tuple
+coordinates = (10, 20)
+print("Tuple:", coordinates)
+print("Type:", type(coordinates))
+print()
+
+# Set
+numbers = {1, 2, 3, 4}
+print("Set:", numbers)
+print("Type:", type(numbers))
+print()
+
+# Dictionary
+student = {
+    "name": "Tamseel",
+    "age": 18,
+    "course": "Python"
+}
+print("Dictionary:", student)
+print("Type:", type(student))
